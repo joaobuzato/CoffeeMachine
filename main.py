52d3a0301c5f7fd0caa65262bbc005535f7a4883
Buzato <joaobuzato@gmail.com>
2022-02-15 09:04:15 -0300

    adding the menu

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
 # TODO: 2. Check if resources are sufficient to make order.
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5
+    },
+    "latte": {
+        "ingredients": {
+            "water": 100,
+            "coffee": 24,
+            "milk": 150
+        },
+        "cost": 2.5
+    },
+    "capuccino": {
+        "ingredients": {
+            "water": 250,
+            "coffee": 24,
+            "milk": 100
+        },
+        "cost": 3.0
+    },
+
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100
+}
